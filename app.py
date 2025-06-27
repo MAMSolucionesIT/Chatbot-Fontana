327bc41ca2f2644c8af46c24a681413d0ae8e9d8
MAMSolucionesIT <130500391+MAMSolucionesIT@users.noreply.github.com>
2025-06-27 12:56:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://fontanacakes.com"],  # O ["*"] para desarrollo
+    allow_origins=["https://fontanacakes.com/"],  # O ["*"] para desarrollo
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
